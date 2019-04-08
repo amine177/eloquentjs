cd88f28b809584a82858cbee0de7755b723c9cc7
aminos <amri.amine@protonmail.com>
2019-04-08 03:19:38 +0200

    added support for a step and reversed ranges

--- ch4/range.js
+++ ch4/range.js
@@ -1,8 +1,13 @@
-function range(start, end) {
+function range(start, end, step=1) {
   var ret = [];
-  for (var i = start; i <= end; ++i)
-    ret.push(i);
+  if (step >= 0)
+    for (let i = start; i <= end; i += step)
+      ret.push(i);
+  else
+    for (let i = start; i >= end; i += step)
+      ret.push(i);
   return ret;
 }
 
 console.log(range(3, 7));
+console.log(range(7, 3, -1));
