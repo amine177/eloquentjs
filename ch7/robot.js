27cabf7a204b47c8f7160b8c05ba7f5439f06409
aminos <amri.amine@protonmail.com>
2019-05-29 22:43:10 +0100

    Created a robot that tries to find good paths - Created findRoute() it tries to explore all routes in order to find one that leads to a place, the problem is that it is redundant in its search - Create goalOrientedRobot() it tries to find a route to the first place that containes parcels parting from the current state's place

--- ch7/robot.js
+++ ch7/robot.js
@@ -105,6 +105,36 @@ function randomPick(array) {
 function randomRobot(state) {
   return {direction: randomPick(roadGraph[state.place])};
 }
+
+
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for(let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+
+
 const roadGraph = buildGraph(roads);
 console.log("Our graph:")
 console.log("**************"); 
@@ -115,4 +145,4 @@ console.log("**************");
 console.log("running random robot");
 let randomVillage = VillageState.random();
 console.log(randomVillage);
-runRobot(randomVillage, randomRobot);
+runRobot(randomVillage, goalOrientedRobot, []);
