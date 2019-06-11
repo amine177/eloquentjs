151a3d5c887c23507ef4ff511d4b512fa930434d
aminos <amri.amine@protonmail.com>
2019-06-11 02:28:10 +0100

    Demonstrating chaning Promise handlers + reject handler - By constructing a Promise that would always reject (the executor function (_, rejectCallback) exectues the rejectCallback) we can then catch the failure and chain to other then handlers as every then/catch returns a Promise

---
+++ ch11/promise-reject.js
@@ -0,0 +1,8 @@
+new Promise(
+  (_dummy, rejectCallback) => rejectCallback(new Error("Async job failed")))
+  .then(value => console.log("First handler"))
+  .catch(reason => {
+    console.log("Failure because : " + reason);
+    return -1;
+  })
+  .then(value => console.log("Second handler", value));
