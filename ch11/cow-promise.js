80ddeec81bf8ee7fe6d195b307be6d27b6cf5ab6
aminos <amri.amine@protonmail.com>
2019-06-11 01:08:05 +0100

    Using a Promise to recover sotred data - Promise takes in its constructor  a function that takes a function used to handle resolved data - To pass the the handler function we must use the then() method on a Promise instance

---
+++ ch11/cow-promise.js
@@ -0,0 +1,10 @@
+const bigOak = require('./crow-tech').bigOak;
+
+function storage(nest, name) {
+  return new Promise(resolve => {
+    nest.readStorage(name, result => resolve(result));
+  });
+}
+
+storage(bigOak, "enemies")
+  .then(value => console.log("Got", value));
