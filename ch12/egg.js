6cf3b2778113c9c61ce1a3952753f03c9843d1ff
aminos <amri.amine@protonmail.com>
2019-08-08 09:28:55 +0100

    added function support

--- ch12/egg.js
+++ ch12/egg.js
@@ -125,7 +125,39 @@ specialForms.define = (args, scope) => {
   return value;
 };
 
+
+specialForms.fun = (args, scope) => {
+  if (!args.length) {
+    throw new SyntaxError("Functions need a body");
+  }
+  let body = args[args.length = 1];
+  let params = args.slice(0, args.length - 1).map(expr => {
+    if (expr.type != "word") {
+      throw new SyntaxError("Parameter names must be words");
+    }
+    return expr.name;
+  });
+
+
+  return function() {
+    if (arguments.length != params.length) {
+      throw new TypeError("Wrong number of arguments");
+    }
+
+    let localScope = Object.create(scope);
+    for (let i = 0; i < arguments.length; i++) {
+      localScope[params[i]] = arguments[i];
+    }
+    return evaluate(body, localScope);
+  };
+};
+
 const topScope = Object.create(null);
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+  topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
 topScope.true = true;
 topScope.false = false;
 let prog = parse(`if(true, false, true)`);
